43d6980df3d24b5dc6fa080d8f0994da4ebed1c0
HelloSunshineHello <2819469231@qq.com>
2019-08-12 16:45:40 +0800

    update

--- LeedcodeTest/src/com/shu/leetcodeTest/Problem6.java
+++ LeedcodeTest/src/com/shu/leetcodeTest/Problem6.java
@@ -1,47 +1,129 @@
 package com.shu.leetcodeTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem6 {
 	public static void main(String[] args) {
-		convert("LEETCODEISHIRING", 3);
+		// convert01("LEETCODEISHIRING", 1);
+		convert03("LEETCODEISHIRING", 3);
+		convert02("LEETCODEISHIRING", 3);
 	}
 
-	public static String convert(String s, int numRows) {
-		int len = s.length();
-		// numColnums= len/(2numRows-2)*(numRows-1)=len/2;
-		int numColnums = (len + 1) / 2;
-		char[][] a = new char[numRows][numColnums];
-		System.out.println("numRows:" + numRows);
-		System.out.println("numColnums:" + numColnums);
-		int k = 0;
-		while (k < len) {
-			for (int i = 0; i < numColnums; i++) {
-				for (int j = 0; j < numRows; j++) {
-					System.out.println("i,j:" + i + "," + j);
-
-					if (i % (numRows - 1) == 0) {
-						a[i][j] = s.charAt(k);
-						System.out.println("k:" + k);
-						k++;
-
-					} else if (i % (numRows - 1) == j) {
-						a[i][j] = s.charAt(k);
-						System.out.println("k:" + k);
-						k++;
-					}
-					System.out.println("a[" + i + "][" + j + "]:" + a[i][j]);
-					System.out.println("----------------");
-				}
-
-			}
+	// 方法二：（推荐）
+	public static String convert02(String s, int numRows) {
+		if (numRows == 1) {
+			System.out.println(s);
+			return s;
 		}
+
+		int len = s.length();
+		boolean goingDown = false;
+		int curRow = 0;
+
+		List<StringBuilder> list = new ArrayList<>();
+		for (int i = 0; i < Math.min(len, numRows); i++) {
+			list.add(new StringBuilder());
+		}
+		// 将每行排好
+		for (char c : s.toCharArray()) {
+			list.get(curRow).append(c);
+			if (curRow == 0 || curRow == numRows - 1)
+				goingDown = !goingDown;
+			curRow += goingDown ? 1 : -1;
+		}
+
 		StringBuilder sb = new StringBuilder();
+		for (StringBuilder strb : list) {
+			sb.append(strb);
+		}
+
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
+
+	// 方法三：
+	public static String convert03(String s, int numRows) {
+		if (numRows == 1) {
+			System.out.println(s);
+			return s;
+		}
+
+		int cycleLen = 2 * numRows - 2;
+		StringBuilder sb = new StringBuilder();
+
 		for (int i = 0; i < numRows; i++) {
-			for (int j = 0; j < numColnums; j++) {
-				sb.append(a[i][j]);
+			System.out.println("i=" + i);
+			for (int j = 0; j + i < s.length(); j += cycleLen) {
+				sb.append(s.charAt(j + i));
+				System.out.println("1:" + s.charAt(j + i));
+				if (i > 0 && i < numRows - 1 && (j - i + cycleLen) < s.length()) {
+					sb.append(s.charAt(j + cycleLen - i));
+					System.out.println("2:" + s.charAt(j + cycleLen - i));
+				}
 			}
+			System.out.println("------------------------");
 		}
 		System.out.println(sb.toString());
 		return sb.toString();
+	}
+
+	// 方法一：自己的二维数组，笨办法
+	/**
+	 * 执行用时 :882 ms, 在所有 Java 提交中击败了5.05% 的用户 内存消耗 :164.1 MB, 在所有 Java
+	 * 提交中击败了5.02%的用户 炫耀一下:
+	 * 
+	 * @param s
+	 * @param numRows
+	 * @return
+	 */
+	public static String convert01(String s, int numRows) {
+		// 当行数为1时 ，直径输出。
+		if (numRows == 1) {
+			System.out.println(s.toString());
+			return s;
+		}
+		int len = s.length();
+
+		// numColnums= len/(2numRows-2)*(numRows-1)=len/2;
+		int numColnums = (len + 1) / 2;
+		char[][] a = new char[numColnums][numRows];
+		System.out.println("numRows:" + numRows);
+		System.out.println("numColnums:" + numColnums);
+		int k = 0;
+
+		for (int i = 0; i < numColnums; i++) {
+			for (int j = 0; j < numRows; j++) {
+				System.out.println("i,j:" + i + "," + j);
+
+				if (i % (numRows - 1) == 0 && k < len) {
+					a[i][j] = s.charAt(k);
+					System.out.println("k:" + k);
+					k++;
+
+				} else if (i % (numRows - 1) + j + 1 == numRows && k < len) {
+					a[i][j] = s.charAt(k);
+					System.out.println("k:" + k);
+					k++;
+				} else {
+					a[i][j] = ' ';
+				}
+
+				System.out.println("a[" + i + "][" + j + "]:" + a[i][j]);
+				System.out.println("----------------");
+			}
+
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int j = 0; j < numRows; j++) {
+			for (int i = 0; i < numColnums; i++) {
+				sb.append(a[i][j]);
+			}
+		}
+		String s1 = sb.toString().replace(" ", "");
+		System.out.println(s1);
+		return s1.toString();
 
 	}
 }
--- LeedcodeTest/src/com/shu/leetcodeTest/Problem7.java
+++ LeedcodeTest/src/com/shu/leetcodeTest/Problem7.java
@@ -2,13 +2,32 @@ package com.shu.leetcodeTest;
 
 public class Problem7 {
 	public static void main(String[] args) {
-		reverse(120);
-
+		reverse(2147483647);
+		reverse02(2147483647);
 	}
 
+	// 方法(推荐）
+	public static int reverse02(int x) {
+		int ret = 0;
+		while (x != 0) {
+			int pop = x % 10;
+			x /= 10;
+
+			if (ret > Integer.MAX_VALUE / 10 || (ret == Integer.MAX_VALUE / 10 && pop > Integer.MAX_VALUE % 10))
+				// System.out.println(0);
+				return 0;
+			if (ret < Integer.MIN_VALUE / 10 || (ret == Integer.MIN_VALUE / 10 && pop < Integer.MIN_VALUE % 10))
+				// System.out.println(0);
+				return 0;
+			ret = ret * 10 + pop;
+		}
+		// System.out.println(ret);
+		return ret;
+	}
+
+	// 自己的思路,没有判断数字益处的问题,存在益处的问题
 	public static int reverse(int a) {
 		StringBuilder sb = new StringBuilder();
-
 		if (a < 0) {
 			sb.append("-");
 			a = -a;
@@ -21,7 +40,14 @@ public class Problem7 {
 			}
 			a /= 10;
 		}
+
 		String s = new String(sb);
-		return Integer.parseInt(s);
+		System.out.println(s);
+		int m = Integer.parseInt(s);
+		if (m > Integer.MAX_VALUE || m < Integer.MIN_VALUE)
+			return 0;
+		// System.out.println(0);
+		return m;
+
 	}
 }
